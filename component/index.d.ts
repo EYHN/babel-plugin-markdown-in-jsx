8f1a3156fe4d35e71d6d15a20bda0f2d3cf69826
eyhn <cneyhn@gmail.com>
2018-11-22 21:42:56 +0800

    add 'inline' props for typescript

--- component/index.d.ts
+++ component/index.d.ts
@@ -1,5 +1,5 @@
 import * as React from "react";
 
-declare function Markdown(props: { children?: React.ReactNode }): React.ReactElement<any>;
+declare function Markdown(props: { children?: React.ReactNode, inline?: boolean }): React.ReactElement<any>;
 
 export default Markdown;
